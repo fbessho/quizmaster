6370914c772dae9ba8a3a34fee7d562aa9870e75
cruz <mike.robintino@gmail.com>
2023-09-28 08:54:48 +0900

    Log in local time

--- wellgo/solution_finder.py
+++ wellgo/solution_finder.py
@@ -24,6 +24,7 @@ logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S %Z",
 )
 logger = logging.getLogger(__name__)
 
