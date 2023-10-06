fb761b3bd72632641806ed892b7191f1dc64e55e
cruz <mike.robintino@gmail.com>
2023-10-06 17:23:38 +0900

    Dial down temperature to 0.2. Lets get some correct answers now ;)

--- wellgo/solution_finder.py
+++ wellgo/solution_finder.py
@@ -110,7 +110,7 @@ def get_prompt(question, answers):
 def determine_answer(qu, answers):
     """Use ChatGPT to solve qu and return answer from answers"""
     response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct", prompt=get_prompt(qu, answers), temperature=1.0
+        model="gpt-3.5-turbo-instruct", prompt=get_prompt(qu, answers), temperature=0.2
     )
     logger.info(response)
     answer = response.choices[0].text
